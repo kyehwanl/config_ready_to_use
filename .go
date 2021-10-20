130e542cd9acea90f1ace50b0ec6178215f96de1
Kyehwan Lee <kyehwanl@nist.gov>
2021-10-20 19:47:24 -0400

    modified .go for GOPATH

--- .go
+++ .go
@@ -1,4 +1,4 @@
 #!/bin/sh
-export GOPATH=/opt/project/gobgp_test/gowork
-#export GOPATH=/opt/project/gobgp_test/gobgp_v2_running_test
+#export GOPATH=/opt/project/gobgp_test/gowork
+export GOPATH=/root/go
 export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin
