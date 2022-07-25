c43e0554629f2b368d2d6766a9cf96cd1ab8f081
Kyehwan Lee <kyehwanl@nist.gov>
2022-07-25 14:09:32 +0000

    In install.sh, modified copy command to avoid error condition

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ yum -y install tmux vim vifm ctags cscope gcc automake autoconf libtool net-tool
 cd ~/
 git clone https://github.com/kyehwanl/config_ready_to_use.git
 unalias cp
-cp -rf config_ready_to_use/.* config_ready_to_use/* ./
+cp -rf config_ready_to_use/.[a-z]* config_ready_to_use/* ./
 rm -rf config_ready_to_use
 find ./ -name '*.tar.gz' | xargs -I % tar xvfz % > /dev/null
 source .bashrc
