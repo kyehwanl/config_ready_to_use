278c536d749c524b81298a0e79aaf9e0a6573570
Kyehwan Lee <kyehwanl@nist.gov>
2025-02-10 16:10:30 -0500

    in vagrant_vim_env_config.sh, modified download command

--- vagrant_vim_env_config.sh
+++ vagrant_vim_env_config.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -x
 
-cd $HOME
+#cd $HOME
 
 # download vim files
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.vimrc
@@ -8,13 +8,16 @@ wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.gitconfig
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.bash_aliases
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.bashrc
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/_.vim_2023_0203.tar.gz 
-tar xvfz _.vim_2023_0203.tar.gz 
+tar xvfz _.vim_2023_0203.tar.gz >/dev/null 2>&1 
+
+cp -rf .vimrc .gitconfig .bash_aliases .bashrc $HOME/
 
 
 # time zone change
 apt update
 command -v sudo >/dev/null 2>&1 || apt install -y sudo 
 command -v timedatectl >/dev/null 2>&1 || sudo apt install -y systemd
+command -v vim >/dev/null 2>&1 || apt install -y vim 
 sudo timedatectl set-timezone America/New_York
 sudo apt install ctags exuberant-ctags cscope -y
 
