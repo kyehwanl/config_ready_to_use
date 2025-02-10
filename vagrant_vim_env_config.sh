1479475fe5bf94f6b10a512a5740689aa38ec4ab
Kyehwan Lee <kyehwanl@nist.gov>
2025-02-10 15:57:28 -0500

    in vagrant_vim_env_config.sh, added install commands

--- vagrant_vim_env_config.sh
+++ vagrant_vim_env_config.sh
@@ -6,13 +6,16 @@ cd $HOME
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.vimrc
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.gitconfig
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.bash_aliases
+wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.bashrc
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/_.vim_2023_0203.tar.gz 
 tar xvfz _.vim_2023_0203.tar.gz 
 
 
 # time zone change
+command -v sudo >/dev/null 2>&1 || apt install -y sudo 
+command -v timedatectl >/dev/null 2>&1 || sudo apt install -y systemd
 sudo timedatectl set-timezone America/New_York
-sudo apt install ctags cscope -y
+sudo apt install ctags exuberant-ctags cscope -y
 
 
 # ENV variables
