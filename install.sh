17b50fe547ecfc158d3046254e17a173faa1b2a3
Kyehwan Lee <kyehwanl@nist.gov>
2025-02-12 11:44:30 -0500

    in install.sh, go delve debugger added

--- install.sh
+++ install.sh
@@ -118,6 +118,10 @@ echo "--- gotags install ---"
 /usr/local/go/bin/go install github.com/jstemmer/gotags@latest
 
 
+echo "--- delve go debugger install ---"
+/usr/local/go/bin/go install github.com/go-delve/delve/cmd/dlv@latest
+
+
 echo "--- Lazygit install ---"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
