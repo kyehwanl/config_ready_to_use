b3fa618c144b41613dcb07c0e813c1635b76c37c
Kyehwan Lee <kyehwanl@nist.gov>
2025-02-21 12:57:48 -0500

    change metallb_install.sh

--- docker_k8s_install_scripts/metallb_install.sh
+++ docker_k8s_install_scripts/metallb_install.sh
@@ -2,28 +2,33 @@
 set -x
 helm repo add metallb https://metallb.github.io/metallb
 helm repo update
-                                                                                              
-helm upgrade --install metallb  metallb/metallb --create-namespace --namespace metallb-system 
-                                                                                              
-kubectl apply -f -<<EOF
-apiVersion: metallb.io/v1beta1                                                              
-kind: IPAddressPool                                                                         
-metadata:                                                                                   
-  name: default-pool                                                                        
-  namespace: metallb-system                                                                 
-spec:                                                                                       
-  addresses:                                                                                
-  - 192.168.1.10-192.168.1.20                                                               
-EOF
-                                                                                              
+helm upgrade --install metallb  metallb/metallb --create-namespace --namespace metallb-system
 
-kubectl apply -f - <<EOF
-apiVersion: metallb.io/v1beta1                                                              
-kind: L2Advertisement                                                                       
-metadata:                                                                                   
-  name: default                                                                             
-  namespace: metallb-system                                                                 
-spec:                                                                                       
-  ipAddressPools:                                                                           
-  - default-pool                                                                            
+sleep 1
+
+cat <<EOF > IPAddressPool.yaml
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: default-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - 192.168.1.10-192.168.1.20
 EOF
+kubectl apply -f IPAddressPool.yaml
+
+
+cat <<EOF > L2Advertisement.yaml
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: default
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+  - default-pool
+EOF
+
+kubectl apply -f L2Advertisement.yaml
+
