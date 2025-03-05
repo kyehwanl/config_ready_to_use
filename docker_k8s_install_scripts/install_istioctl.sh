f65560d15b06a62d157efb2e8e7696630069c374
Kyehwan Lee <kyehwanl@nist.gov>
2025-03-05 09:56:20 -0500

    modified install_istioctl.sh for fixing version

--- docker_k8s_install_scripts/install_istioctl.sh
+++ docker_k8s_install_scripts/install_istioctl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-curl -L https://istio.io/downloadIstio | sh -
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.24.3 sh -
 cd istio-1.24.3
 sudo install -m 0755 bin/istioctl /usr/local/bin/istioctl
 
